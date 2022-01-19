b8f63ab726e81aed966ca93543f4acb48bfe7c5b
Venkat  R <venkygowdru99@gmail.com>
2022-01-19 12:41:39 +0530

    .

--- components/navbar.js
+++ components/navbar.js
@@ -1,29 +1,6 @@
-function navbar() {
-  return `
-    <div id="logo">
-        <img
-          src="./images/download.png"
-          alt="logo"
-        />
-      </div>
-      <div id="lct">
-        <p><i class="fas fa-map-marker-alt"></i> Mumbai 400000</p>
-      </div>
-      <div id="form">
-        <input
-          type="text"
-          id="search"
-          placeholder="What are you looking for ?"
-        />
-        <button type="submit" id="btn">Search</button>
-      </div>
-      <div id="signin">
-        <a href=""><i class="far fa-user"></i>Sign In/Register</a>
-      </div>
-      <div id="cart">
-        <a href=""> <i class="fas fa-shopping-cart"></i>Cart</a>
-      </div>
-    `;
+let navbar=()=>{
+    return `
+      <img src="images/logo.jpg" alt="Dmart-logo" />
+      <div>Signed in as</div>`;
 }
-
-export default navbar;
+export {navbar}
