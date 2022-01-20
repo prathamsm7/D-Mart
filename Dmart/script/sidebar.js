49cb80060761fb788ac2f8a5585673e0a575519b
RUPESH RAIKWAR <96075514+RupeshRaikwar@users.noreply.github.com>
2022-01-20 11:26:04 +0530

    Add files via upload

---
+++ Dmart/script/sidebar.js
@@ -0,0 +1,9 @@
+function sidebar(){
+    return `   <div id="ul1">Account Details</div>
+    <a href="profile.html"><div id="li1">My Profile</div></a>
+    <a href="myAddress.html"><div id="li2">My Address</div></a>
+    <a href="ready.html"><div id="ul2">Ready List</div></a>
+    <div id="ul3">My Order</div>`;
+}
+
+export default sidebar ;
