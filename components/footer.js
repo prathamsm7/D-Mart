afe251e3ff7f133af41f61f88a8e0d4378a2214f
birendra20 <96100549+birendra20@users.noreply.github.com>
2022-01-21 19:12:11 +0530

    Add files via upload

---
+++ components/footer.js
@@ -0,0 +1,26 @@
+function footer() {
+  return `
+        <div id="f_one">
+            <p id="line_t">Download DMart Ready Mobile App Now!!</p>
+            <div id="gp_apl">
+                <img id="gp_img" src="https://img.dmart.in/images/rwd/footer/google-play.svg" alt=""><img id="apl_img" src="https://img.dmart.in/images/rwd/footer/download-app-store.svg" alt="">
+            </div>
+            <div id="small_img">
+                <img id="a1" src="https://img.dmart.in/images/rwd/footer/visa.svg" alt=""><img id="a2" src="https://img.dmart.in/images/rwd/footer/mastercard.svg" alt=""><img id="a3" src="https://img.dmart.in/images/rwd/footer/american.svg" alt=""><img id="a4" src="https://img.dmart.in/images/rwd/footer/rupay.svg" alt=""><img id="a5" src="https://img.dmart.in/images/rwd/footer/cash_outlined.svg" alt="">
+            </div>
+        </div>
+        <div id=f_two>
+            <p id="ph">FAQs</p>
+            <p id="ph">Privacy Policies</p>
+            <p id="ph">Returns & Refunds</p>
+            <p id="ph">Terms & Conditions</p>
+        </div>
+        <div id="f_three">
+            <p id="ph">Contact Us</p>
+            <p id="ph">Pickup Points</p>
+        </div>
+       
+    `;
+}
+
+export default footer;
