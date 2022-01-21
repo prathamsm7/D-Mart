f154237a4224d09786ba8976ab61196b866d8c4e
doulath123 <96289292+doulath123@users.noreply.github.com>
2022-01-21 21:50:59 +0530

    updated

---
+++ Doulath/fruits&veg.js
@@ -0,0 +1,278 @@
+var data = [
+    {
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUIT4pcsTGBM22XX281018_2_P.jpg",
+        name:"Fresh Pear (Nashpati) : 4 Pieces(Approx. 575gm - 650gm)",
+        price:131,
+        strikedOffPrice:160
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUITS4pcs97XX141018_3_P.jpg",
+        name:"Fresh Pomegranate (Anar) : 4 Pieces (Approx. 0.8 - 1 kg)",
+        price:159,
+        strikedOffPrice:250
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/S/S/KSSONFRUITS4PcsFres2xx160319_3_P.jpg",
+        name:"Apple Royal Gala (Seb) : 4 Pieces (Approx. 500 - 750 gm)",
+        price:135,
+        strikedOffPrice:225
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUIT4pcs14xx230920_5_P.jpg",
+        name:"Fresh Orange (Santra) Imported : 4 Pieces (Approx. 800 - 900 gm)",
+        price:110,
+        strikedOffPrice:150
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUIT1pcs18XX281018_3_P.jpg",
+        name:"Fresh Watermelon (Tarbooj) : 1 Piece (Approx. 2.50 - 3 kg)",
+        price:89,
+        strikedOffPrice:150
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/D/E/C/DEC250000179xx8DEC21_5_P.jpg",
+        name:"Apple Red Delicious : 4 Pieces",
+        price:131,
+        strikedOffPrice:180
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRUITS500gm14xx230920_5_P.jpg",
+        name:"Fresh Banana (Kela) Elaichi : 6 Pieces",
+        price:46,
+        strikedOffPrice:65
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUITS1pc98XX141018_1_P.jpg",
+        name:"Fresh Papaya (Papita) : 1 Piece (Approx. 650 - 750gm)",
+        price:35,
+        strikedOffPrice:45
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/L/F/R/LFRESHVEGE4U02xx230920_5_P.jpg",
+        name:"Fresh Sweet Lime (Mosambi) : 4 Pieces (Approx. 500 - 650gm)",
+        price:42,
+        strikedOffPrice:50
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRUITS3PCS2xx230920_5_P.jpg",
+        name:"Fresh Kiwi: 3 Pieces",
+        price:62,
+        strikedOffPrice:75
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUIT1pcs20xx230920_5_P.jpg",
+        name:"Fresh Muskmelon (Kharbooja) : 1 Piece (Approx. 0.7 - 1 kg)",
+        price:54,
+        strikedOffPrice:75
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/L/S/S/LSSONFRUITS4U016xx230120_5_P.jpg",
+        name:"Fresh Apple Green : 4 Pieces",
+        price:179,
+        strikedOffPrice:275
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/L/F/R/LFRUITS4Pcs02xx100521_5_P.jpg",
+        name:"Fresh Apple Fuji : 4 Pieces(Approx. 650gm - 700gm )",
+        price:175,
+        strikedOffPrice:250
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/L/S/S/LSSONFRUITS4U04xx230120_5_P.jpg",
+        name:"Fresh Guava (Peru) : 4 Pieces (Approx. 450gm - 500gm)",
+        price:45,
+        strikedOffPrice:55
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRUITS1pc9XX201118_1_P.jpg",
+        name:"Fresh Dragon Fruit (Pitaya) : 1 Piece(Approx. 300gm - 400gm)",
+        price:82,
+        strikedOffPrice:95
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/L/S/S/LSSONFRUITS1UDoms60xx271219_5_P.jpg",
+        name:"Sweet Tamarind (Thailand) : 200 gms",
+        price:105,
+        strikedOffPrice:150
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRUITS400gm3xx230920_5_P.jpg",
+        name:"Fresh Raw Mango : 400 gms",
+        price:85,
+        strikedOffPrice:100
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUITS6pcs94XX141018_9_P.jpg",
+        name:"Fresh Orange (Santra) Big : 6 Pieces (Approx. 600 - 700gm)",
+        price:99,
+        strikedOffPrice:125
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRUITS250gm8XX261218_2_P.jpg",
+        name:"Fresh Indian Gooseberry (Amla) : 250 gms",
+        price:26,
+        strikedOffPrice:35
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRUITS4pcs12xx230920_5_P.jpg",
+        name:"Raw Banana (Kela) Madras : 4 Pieces",
+        price:35,
+        strikedOffPrice:55
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/L/F/R/LFRUITS6pcs056xx310521_5_P.jpg",
+        name:"Fresh Sapota (Chikku) : 6 Pieces",
+        price:38,
+        strikedOffPrice:60
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRUITS1pc7XX31218_1_P.jpg",
+        name:"Fresh Pineapple (Ananas) : 1 Piece (Approx. 775gm - 850gm)",
+        price:40,
+        strikedOffPrice:60
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/O/C/T/OCT250000040xx7OCT21_5_P.jpg",
+        name:"Robusta Banana (Kela) : 6 Pieces",
+        price:30,
+        strikedOffPrice:40
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRUITS500gm13XX40618_2_P.jpg",
+        name:"Sonaka Fresh Grapes (Draksha) : 500 gms",
+        price:65,
+        strikedOffPrice:100
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUITS6pcs99XX141018_1_P.jpg",
+        name:"Fresh Apple (Seb) Kinnaur : 4 Pieces (Approx. 0.6 - 0.8 kg)",
+        price:110,
+        strikedOffPrice:175
+    },{
+        imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRUITS6PCS6XX91218_5_P.jpg",
+        name:"Fresh Kinnow Orange (Santra) : 6 Pieces (Approx. 0.9 - 1.25 kg)",
+        price:110,
+        strikedOffPrice:150
+    },
+];
+var fruit=[{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUIT4pcsTGBM22XX281018_2_P.jpg",
+    name:"Fresh Pear (Nashpati) : 4 Pieces(Approx. 575gm - 650gm)",
+    price:131,
+    strikedOffPrice:160
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUITS4pcs97XX141018_3_P.jpg",
+    name:"Fresh Pomegranate (Anar) : 4 Pieces (Approx. 0.8 - 1 kg)",
+    price:159,
+    strikedOffPrice:250
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/S/S/KSSONFRUITS4PcsFres2xx160319_3_P.jpg",
+    name:"Apple Royal Gala (Seb) : 4 Pieces (Approx. 500 - 750 gm)",
+    price:135,
+    strikedOffPrice:225
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUIT4pcs14xx230920_5_P.jpg",
+    name:"Fresh Orange (Santra) Imported : 4 Pieces (Approx. 800 - 900 gm)",
+    price:110,
+    strikedOffPrice:150
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/F/R/KFRESHFRUIT1pcs18XX281018_3_P.jpg",
+    name:"Fresh Watermelon (Tarbooj) : 1 Piece (Approx. 2.50 - 3 kg)",
+    price:89,
+    strikedOffPrice:150
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/D/E/C/DEC250000179xx8DEC21_5_P.jpg",
+    name:"Apple Red Delicious : 4 Pieces",
+    price:131,
+    strikedOffPrice:180
+},];
+var vegetable=[{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/O/N/KONIONS2kg22XX220318_1_P.jpg",
+    name:"Onion : 3 kgs",
+    price:114,
+    strikedOffPrice:150
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/L/B/O/LBOTTLES1kgFres22xx100120_7_P.jpg",
+    name:"Freshezza Frozen Green Peas : 1 kgs",
+    price:130,
+    strikedOffPrice:260
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/P/O/KPOTATO1kg3132XX270118_1_P.jpg",
+    name:"Fresh Potato (Aloo) : 2 kgs",
+    price:50,
+    strikedOffPrice:70
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/C/O/KCOCONUT1pcs3134XX160818_1_P.jpg",
+    name:"Coconut : 1 Unit",
+    price:32,
+    strikedOffPrice:50
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/V/E/KVEGETABLES500gm41xx230920_5_P.jpg",
+    name:"Fresh Tomato (Tamaatar) : 500 gms",
+    price:23,
+    strikedOffPrice:40
+},{
+    imgUrl:"https://img.dmart.in/images/rwd/products/K/O/T/KOTHERVEGETABLE200GM16XX210218_1_P.jpg",
+    name:"Ginger (Adrak) : 200 gms",
+    price:15,
+    strikedOffPrice:25
+},];
+dou(data)
+cart= JSON.parse(localStorage.getItem("cartdatabase"))||[];
+function dou(data){ 
+   document.querySelector("#fruits").style.color="black"
+   document.querySelector("#vegetables").style.color="black"
+
+   data.map(function (elem) {
+    var mainDiv=document.createElement("button");
+    mainDiv.setAttribute("id", "mainDiv")
+    var img=document.createElement("img");
+    img.setAttribute("src",elem.imgUrl);
+    var veg=document.createElement("img")
+    veg.setAttribute("id","veg");
+    veg.src="https://content.dmart.in/site/static/media/veg.1a9a7a12.svg"
+    var imgdiv=document.createElement("div")
+    imgdiv.setAttribute("id","imgdiv")
+    imgdiv.append(img,veg)
+    var h4=document.createElement("h4");
+    h4.textContent=elem.name;
+    var but=document.createElement("button");
+    but.setAttribute("id","but")
+    but.textContent="🛒  ADD TO CART"
+    var smallDiv=document.createElement("div");
+    smallDiv.setAttribute("class", "smallDiv");
+    var price=document.createElement("p");
+    price.textContent = ("₹ "+elem.price)
+    price.setAttribute("class","price")
+    var dmart=document.createElement("p")
+    dmart.textContent="DMart";
+    var DMart=document.createElement("div")
+    DMart.append(dmart,price)
+    var strikedOffPrice=document.createElement("p");
+    strikedOffPrice.setAttribute("class","price2");
+    strikedOffPrice.textContent= "₹ "+elem.strikedOffPrice
+    var mrp=document.createElement("p")
+     mrp.textContent="MRP";
+     var MRP=document.createElement("div")
+     MRP.append(mrp,strikedOffPrice);
+     MRP.style.marginRight="25%"
+     var tax=document.createElement("p")
+    tax.textContent="(Inclusive of all taxes)";
+    tax.style.fontStyle="italic";
+    tax.style.marginTop="-15px"
+    smallDiv.append(MRP,DMart);
+    var save=document.createElement("p")
+    save.textContent="Save"
+    save.style.marginTop="5px"
+    var saveprice=document.createElement("p")
+    saveprice.textContent="₹ "+(elem.strikedOffPrice-elem.price)
+saveprice.style.fontSize="20px"
+saveprice.style.marginTop="-15px"
+    var Save=document.createElement("div")
+    Save.append(save,saveprice)
+   Save.setAttribute("id","price3")
+    var Tax=document.createElement("div")
+    Tax.append(smallDiv,tax)
+    Tax.setAttribute("id","Tax")
+  var SAVE=document.createElement("div")
+  SAVE.append(Tax,Save)
+    SAVE.style.display="flex"
+    smallDiv.style.display="flex"
+    mainDiv.append(imgdiv,h4,SAVE,but);
+    document.getElementById("con").append(mainDiv)
+    but.addEventListener("click", function () {
+        cart.push(elem);
+        localStorage.setItem("cartdatabase", JSON.stringify(cart))
+        console.log(cart)
+   })
+})};
+function fruits(){
+    document.querySelector("#con").textContent="";
+    document.getElementById("h1").textContent="Fresh Fruits" 
+   dou(fruit)
+   document.querySelector("#fruits").style.color="green" 
+}
+function vegetables(){
+    document.querySelector("#con").textContent="";
+    document.getElementById("h1").textContent="Vegetables"
+   dou(vegetable)
+   document.querySelector("#vegetables").style.color="green" 
+}
+
+
