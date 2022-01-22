af36e07662a6137c36e03d5260c19d95b7112335
prathamsm7 <79472183+prathamsm7@users.noreply.github.com>
2022-01-22 20:12:43 +0530

    updated

---
+++ scripts/nav.js
@@ -0,0 +1,9 @@
+function nav() {
+  return ` <div id="nav">
+    <a href="./grocery.html"><p>DMart Grocery</p></a>
+    <a href="./cook&serve.html"> <p>Cookware & Serveware</p></a>
+    <a href="./fruits&veg.html"><p>Fruits & Vegetables</p></a>
+    <a href="./kitchen.html"><p>Kitchen Appliances</p></a>
+</div> `;
+}
+export default nav;
