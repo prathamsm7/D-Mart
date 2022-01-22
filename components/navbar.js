4c5f3d53dfd23ab8f564991c3f8b2d2c39774a42
Venkat  R <venkygowdru99@gmail.com>
2022-01-22 15:37:51 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -55,7 +55,7 @@ function navbar() {
       </div>
 
       <div id="signin">
-        <a href=""><i class="far fa-user"></i><span>Sign In/Register</span></a>
+        <a href="signup.html"><i class="far fa-user"></i><span>Sign In/Register</span></a>
       </div>
       <div id="cart">
         <a href="Cart.html"> <i class="fas fa-shopping-cart"></i><sup  id="crTotal"></sup><span>Cart</span></a>
