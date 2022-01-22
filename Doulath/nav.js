d0047467ce87ccfe334f9052dc4e5bdb0214c6ff
Venkat  R <venkygowdru99@gmail.com>
2022-01-22 17:00:44 +0530

    Completed

--- Doulath/nav.js
+++
@@ -1,9 +0,0 @@
-function nav() {
-  return ` <div id="nav">
-    <a href="./grocery.html"><h3>DMart Grocery</h3></a>
-    <a href="./cook&serve.html"> <h3>Cookware & Serveware</h3></a>
-    <a href="./fruits&veg.html"><h3>Fruits & Vegetables</h3></a>
-    <a href="./kitchen.html"><h3>Kitchen Appliances</h3></a>
-</div> `;
-}
-export default nav;
--- final DMart/script/sidebar.js
+++
@@ -1,9 +0,0 @@
-function sidebar(){
-    return `   <div id="ul1">Account Details</div>
-    <a href="profile.html"><div id="li1">My Profile</div></a>
-    <a href="myAddress.html"><div id="li2">My Address</div></a>
-    <a href="ready.html"><div id="ul2">Ready List</div></a>
-    <a href="cart.html"><div id="ul3">My Order</div></a>`;
-}
-
-export default sidebar ;
