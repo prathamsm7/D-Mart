48d50fd68fc8e7b26b7f7c8288c1a6e85afb1809
Venkat  R <venkygowdru99@gmail.com>
2022-01-22 09:50:37 +0530

    updated

--- components/navbar-pay.js
+++ components/navbar-pay.js
@@ -1,7 +1,7 @@
 let navbar = () => {
   return `
       <img src="images/logo.jpg" alt="Dmart-logo" id="logo-click" />
-      <div>Signed in as <span id="nav-name">Venkat</span></div>
+      <div>Signed in as <span id="nav-name"></span></div>
      `;
 };
 export { navbar };
